54426d3e96f1115f0a21b05da24801b9fb88cf69
Vladyslav <amberv.mail@gmail.com>
2015-11-22 19:44:29 +0100

    steps 1-3 done

---
+++ run_analysis.R
@@ -0,0 +1,33 @@
+#function used to move columns to first position in a data frame
+movetofirst <- function(data, move) {
+  data[c(move, setdiff(names(data), move))]
+}
+#install dplyr if not installed and load
+if("dplyr" %in% rownames(installed.packages()) == FALSE) {install.packages("dplyr")}
+library(dplyr)
+#read data
+subject_test <- read.table("data/test/subject_test.txt", stringsAsFactors=FALSE)
+subject_train <- read.table("data/train/subject_train.txt", stringsAsFactors=FALSE)
+X_test <- read.table("data/test/X_test.txt", stringsAsFactors=FALSE)
+X_train <- read.table("data/train/X_train.txt", stringsAsFactors=FALSE)
+y_test <- read.table("data/test/y_test.txt", stringsAsFactors=FALSE)
+y_train <- read.table("data/train/y_train.txt", stringsAsFactors=FALSE)
+activity_labels <- read.table("data/activity_labels.txt", stringsAsFactors=FALSE)
+features <- read.table("data/features.txt", stringsAsFactors=FALSE)
+#merge test and train data
+subject <- rbind(subject_test, subject_train)
+data <- rbind(X_test, X_train)
+y <- rbind(y_test, y_train)
+#properly name variables
+names(data) <- make.names(features[, 2], unique = TRUE)
+#keep only mean and std variables
+data <- select(data, contains("mean"), contains("std"))
+#add subject and activity columns
+data$subject <- subject[[1]]
+data$activity <- as.factor(apply(y, MARGIN = 2, FUN = function(x) activity_labels[x, 2]))
+#move activity and subject columns to beginning
+data <- movetofirst(data, "activity")
+data <- movetofirst(data, "subject")
+#clean up the workspace
+remove(subject_test, subject_train, X_test, X_train, y_test, y_train)
+remove(y, subject, features, activity_labels)
